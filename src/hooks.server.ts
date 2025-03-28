3ca24c2c64d8c63d9f1b908cb0b25e277bd358ce
MoeDev <143090643+MoeDevelops@users.noreply.github.com>
2025-03-28 21:22:01 +0100

    Add sqlite

---
+++ src/hooks.server.ts
@@ -0,0 +1,3 @@
+import { setup } from "$lib/sql/sqlClient"
+
+setup()
---
+++ src/lib/sql/sqlClient.ts
@@ -0,0 +1,5 @@
+import Database from "better-sqlite3"
+
+export const db = new Database("./pwa-playground.db")
+
+export function setup() {}
